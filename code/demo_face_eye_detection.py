cb8d2069e8bb06bc870534f040eeab87a2e7e5c9
Owen Wang <ojwang@stanford.edu>
2017-05-14 21:33:10 -0700

    Prototype class for detection is done. About to test it on corn

--- code/demo_face_eye_detection.py
+++ code/demo_face_eye_detection.py
@@ -1,15 +1,15 @@
+"""
 import numpy as np
 import cv2
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from gazelle_utils import *
+"""
+
+from face_eye_detector import *
 
 # For now, please run this only in the gazelle/code directory.
-
-# Input and Output file names
-face_in  = "face1_macron.jpg"
-face_out = "face1_macron_out.png"
 
 
 """
@@ -17,26 +17,44 @@ Techniques for how to detect faces and eyes drawn from:
 http://docs.opencv.org/trunk/d7/d8b/tutorial_py_face_detection.html
 """
 
-face_finder = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_frontalface_default.xml")
-eye_finder  = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_eye.xml")
+def original_demo():
+    # Input and Output file names
+    face_in  = "face1_macron.jpg"
+    face_out = "face1_macron_out.png"
 
-img  = plt.imread(IMG_DIR + face_in, 1) # for display
-mono = plt.imread(IMG_DIR + face_in, 0) # for cascades
+    face_finder = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_frontalface_default.xml")
+    eye_finder  = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_eye.xml")
 
-# Detection step.
-faces = face_finder.detectMultiScale(mono, 1.3, 5)
-for (x,y,w,h) in faces:
-    # Red bounding box around the face in |img|
-    cv2.rectangle(img, (x,y), (x+w, y+h), color=(255,0,0), thickness=2)
-    gray_region = mono[y:y+h, x:x+w]
-    color_region = img[y:y+h, x:x+w]
-    eyes = eye_finder.detectMultiScale(gray_region)
-    for (ex,ey,ew,eh) in eyes:
-        # Green bounding box around the eyes in |img|
-        cv2.rectangle(color_region, (ex,ey), (ex+ew, ey+eh), color=(0,255,0), thickness=2)
+    img  = plt.imread(IMG_DIR + face_in, 1) # for display
+    mono = plt.imread(IMG_DIR + face_in, 0) # for cascades
+
+    # Detection step.
+    faces = face_finder.detectMultiScale(mono, 1.3, 5)
+    for (x,y,w,h) in faces:
+        # Red bounding box around the face in |img|
+        cv2.rectangle(img, (x,y), (x+w, y+h), color=(255,0,0), thickness=2)
+        gray_region = mono[y:y+h, x:x+w]
+        color_region = img[y:y+h, x:x+w]
+        eyes = eye_finder.detectMultiScale(gray_region)
+        for (ex,ey,ew,eh) in eyes:
+            # Green bounding box around the eyes in |img|
+            cv2.rectangle(color_region, (ex,ey), (ex+ew, ey+eh), color=(0,255,0), thickness=2)
+
+    plt.imshow(img)
+    plt.savefig(IMG_DIR + face_out)
 
 
-plt.imshow(img)
-plt.savefig(IMG_DIR + face_out)
+def detector_class_demo():
+    save_name = "face3_feifei_out.png"
+    face_in_path  = IMG_DIR + "face3_feifei.jpg"
+    face_out_path = IMG_DIR + save_name
+    
+    detector = Detector(face_in_path, in_color=True, run=True)
+    detector.draw_and_save(face_out_path)
+
+
+
+if __name__ == "__main__":
+    detector_class_demo()
 
 
--- code/face_eye_detector.py
+++ code/face_eye_detector.py
@@ -7,44 +7,64 @@ import matplotlib.pyplot as plt
 
 class Detector(object):
 
-    def __init__(self):
+    def __init__(self, input_path, in_color=False, run=False, save=False):
+        """ self.img_color = image in color, after |load_image|
+            self.img_mono  = image in monochrome, after |load_image|
+            self.boxes = 3-list of (x,y,w,h) tuples for face, eye1, eye2 after |get_face| is run
+        """
+        self.img_mono = None
+        self.img_color = None
+        self.boxes = None
         self.face_finder = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_frontalface_default.xml")
         self.eye_finder  = cv2.CascadeClassifier(HAAR_DIR + "haarcascade_eye.xml")
+        load_image(input_path, in_color)
 
-    def _load_image(self, input_path, color=False):
-        if color:
+
+    def load_image(self, input_path, in_color=False):
+        if in_color:
             self.img_color = plt.imread(input_path, 1) # for display purposes
         self.img_mono  = plt.imread(input_path, 0) # for cascade algorithm
 
-    def _get_face(self):
-        """
-        Returns the face and eye bounding boxes as (x,y,w,h) where
-        x,y = coordinates of upper left corner
-        w,h = width and height respectively.
 
-        The image is discarded if we detect more than 1 face or more than 2 eyes.
-        :return: list of len 3 of (x,y,w,h) tuples,
-                 or None if data is noisy.
+    def get_face_and_eyes(self):
+        """ Returns the face and eye bounding boxes as (x,y,w,h) where
+            x,y = coordinates of upper left corner
+            w,h = width and height respectively.
+
+            The image is discarded if we detect more than 1 face or more than 2 eyes.
+            :return: list of len 3 of (x,y,w,h) tuples,
+                     or None if data is noisy.
         """
-        faces = self.face_finder.detectMultiScale(self.mono, 1.3, 5)
-        
+        assert len(self.img_mono.shape) == 3
+
+        faces = self.face_finder.detectMultiScale(self.img_mono, 1.3, 5)
         if len(faces) != 1: return "[Face Error: len %s]" % len(faces)
 
         fx, fy, fw, fh = faces[0]
-        region = self.mono[fy : fy+fh, fx : fx+fw]
-        eyes = self.eye_finder.detectMultiScale(region)
-        
+        face_region = self.img_mono[fy : fy+fh, fx : fx+fw]
+        eyes = self.eye_finder.detectMultiScale(face_region)
         if len(eyes) != 2: return "[Eye Error: len %s]" % len(eyes)
 
-        boxes = [(fx, fy, fw, fh)] + eyes
-        assert len(boxes) == 3
-        return boxes
+        self.boxes = [(fx, fy, fw, fh)] + eyes # check this - actually 3-list of 4-tuples?
+        assert len(self.boxes) == 3
+        return self.boxes
 
-# +========================
 
-    def get_face_and_eyes(self, input_path):
+    def draw_and_save(self, save_path):
+        """ Saves the image associated with this Detector into directory img/
+            with the given name.
         """
-        User facing function.
-        """
-        _load_image(input_path)
-        return _get_face()
+        assert len(self.img_color.shape) == 3
+        assert len(self.boxes) == 3
+        
+        fx, fy, fw, fh = self.boxes[0]
+        # red box around the face
+        cv2.rectangle(self.img_color, (fx,fy), (fx+fw, fy+fh), color=(255,0,0), thickness=2)
+        # green boxes around the eyes
+        for x,y,w,h in self.boxes[1:]:
+            cv2.rectangle(self.img_color, (x,y), (x+w, y+h), color=(0,255,0), thickness=2)
+
+        plt.imshow(self.img_color)
+        plt.savefig(save_path)
+
+
