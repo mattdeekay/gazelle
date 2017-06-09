97699fb98acbc9ff8d01defb2f290027a9019180
mattdeekay <matthewdkim7@gmail.com>
2017-06-09 06:44:02 +0000

    Test added

--- CNN_code/iterate_cnn_epochs.py
+++ CNN_code/iterate_cnn_epochs.py
@@ -29,7 +29,7 @@ def run_one_batch(mode, batch_num_train, batch_num_eval):
 """ Run one epoch of training (i.e. go through all the training data). """
 def run_one_epoch(mode, fileNums, batch_num_eval):
     for f in fileNums:
-        print "  Running fileno ", f, "."
+        print "  ", mode, ": running fileno ", f, "."
         run_one_batch(mode, f, batch_num_eval)
 
 def main(argv):
@@ -53,11 +53,18 @@ def main(argv):
     for f in onlyfiles:
         fileNums.append(int(re.sub("[^0-9]", "", f)))
     fileNums = list(set(fileNums))
+    valNum = fileNums[-2]
+    testNum = fileNums[-1]
+    fileNums = fileNums[:-2]
     
     n_epochs = 10
+    # 'train' 'test' 'validation'
     for i in xrange(n_epochs):
         print "Running epoch ", i, "."
         run_one_epoch('train', fileNums, 0)
+        
+    run_one_epoch('validation', 0, valNum)
+    run_one_epoch('test', 0, testNum)
 
 if __name__ == "__main__":
     tf.app.run()
